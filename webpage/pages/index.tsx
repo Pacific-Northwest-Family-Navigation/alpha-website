79d7d54d4f729c2a5f4a7d735c1e1b5e83bc9d23
carmelitatalty <carmelitawarby@gmail.com>
2024-12-06 23:47:20 -0800

    Minor update

--- webpage/pages/index.tsx
+++ webpage/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <Image src="/logo.png" alt="Pacific Northwest Family Navigation Logo" width={200} height={200} />
+      <Image src="logo.png" alt="Pacific Northwest Family Navigation Logo" width={200} height={200} />
       <h1 className="mt-8 text-4xl font-bold text-center">
         Pacific Northwest Family Navigation
       </h1>
