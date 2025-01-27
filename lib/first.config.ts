48ffebf9200c40047e152eb8dd41d7365ba37103
carmelitatalty <carmelitawarby@gmail.com>
2025-01-26 22:39:08 -0800

    Attempt to fix

---
+++ lib/first.config.ts
@@ -0,0 +1,7 @@
+import { loadFile, writeFile } from "magicast";
+const fileName = "./next.config.js";
+const mod = await loadFile(fileName);
+console.log("Current Code:\n", mod.$code);
+mod.exports.default = {}
+await writeFile(mod, fileName);
+console.info(`Updated ${fileName} with basic configuration`);
