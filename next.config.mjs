6da6385917a322a49dfc885b64705756a2669824
Ben Warby <bencwarby@gmail.com>
2025-01-27 16:43:58 -0800

    Minor

--- next.config.mjs
+++ next.config.mjs
@@ -11,12 +11,12 @@ const nextConfig = {
 
   // Configure a base path
   // https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-  basePath: path,
+  // basePath: path,
 
   // Disable server side image optimization too
   // https://nextjs.org/docs/api-reference/next/image#unoptimized
   'images.unoptimized': true,
-  
+
   // Support Next.js config wrappers, such as next-pwa
   // https://github.com/actions/configure-pages/issues/44#issuecomment-1705407518
   allowWrappingCall: true,
