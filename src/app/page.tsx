1d7911a3a47998e3f76b3add13f1ab23c33811bb
carmelitatalty <carmelitawarby@gmail.com>
2025-03-15 09:12:42 -0700

    Resource Fair day

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
-import Spotlight from "@/components/spotlights/spotlight";
+// import Spotlight from "@/components/spotlights/spotlight";
 import { getCurrentSpotlight } from "@/utils/spotlights-util";
 
 export default function Home() {
   const currentSpotlight = getCurrentSpotlight();
   if (!currentSpotlight) {
-    return (<></>)
+    return <></>;
   }
   return (
     <div>
-      <Spotlight spotlight={currentSpotlight} />
+      <h1 className="text-2xl font-bold text-center">Resource Fair</h1>
+      Hey everyone! Our resource fair is today. Please fill out the following
+      form if you are here.
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSftlAaPe1nws2lUu56c3OeIzn3QFzIaRnlJ5uRs4XeydJStEw/viewform?embedded=true"
+        width="640"
+        height="1302"
+      >
+        Loading…
+      </iframe>
+      {/* <Spotlight spotlight={currentSpotlight} /> */}
     </div>
   );
 }
