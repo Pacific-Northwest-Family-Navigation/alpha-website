337d6beb60892fc48fbf317e3b3e6fe1cfc8e921
carmelitatalty <carmelitawarby@gmail.com>
2025-03-22 17:37:38 -0700

    change main page to be spotlight not resource fair

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// import Spotlight from "@/components/spotlights/spotlight";
+import Spotlight from "@/components/spotlights/spotlight";
 import { getCurrentSpotlight } from "@/utils/spotlights-util";
 
 export default function Home() {
@@ -8,17 +8,7 @@ export default function Home() {
   }
   return (
     <div>
-      <h1 className="text-2xl font-bold text-center">Resource Fair</h1>
-      Hey everyone! Our resource fair is today. Please fill out the following
-      form if you are here.
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSftlAaPe1nws2lUu56c3OeIzn3QFzIaRnlJ5uRs4XeydJStEw/viewform?embedded=true"
-        width="640"
-        height="1302"
-      >
-        Loading…
-      </iframe>
-      {/* <Spotlight spotlight={currentSpotlight} /> */}
+      <Spotlight spotlight={currentSpotlight} />
     </div>
   );
 }
